bb2b217c40488096a5384683df66cd4af42e65a0
aalorbe <aalorbe@sandia.gov>
2019-11-03 20:05:53 -0800

    "jefferson_quote.py: Initial commit"

---
+++ jefferson_quote.py
@@ -0,0 +1,5 @@
+[
+  ["Jefferson Quote",
+  "In matters of principle, stand like a rock; in matters of taste, swim with the current. Give up money, give up fame, give up science, give up earth itself and all it contains, rather than do an immoral act. And never suppose that in any situation, or under any circumstances, it is best for you to do a dishonorable thing. Whenever you are to do a thing, though it can never be known but to yourself, ask yourself how you would act were all the world looking at you, and act accordingly."]
+   ]
+
