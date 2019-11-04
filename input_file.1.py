1845fda32e80962f6eee3d9dec9f51da4e37b99f
aalorbe <aalorbe@sandia.gov>
2019-11-03 21:02:50 -0800

    "input_file.1.py: Added directions and sample comments"

--- input_file.1.py
+++ input_file.1.py
@@ -1,9 +1,24 @@
+# Format:
+# [
+#   ["Question 1",
+#    "Answer 1"],
+#   ["Question 2",
+#    "Answer 2"],
+#   ["Question 3",
+#    "Answer 3"],
+#   ...
+#   ["Question n",
+#    "Answer n"],
+# ]
+
 [
   ["Daugher",
    "Marlene"],
+
+  # This is a comment
   ["Wing Commander",
    "Cadet Colonel Bryant K. Ashe"],
-  ["10x2",
+
+  ["10x2", # This is a comment
    "20"],
 ]
-    
